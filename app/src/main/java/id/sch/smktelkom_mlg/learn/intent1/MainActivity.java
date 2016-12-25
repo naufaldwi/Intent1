9fa05f052eff5561eb47b8a82a0d3ca531f77f41
Naufal Dwi <naufal_prakoso_24rpl@student.smktelkom-mlg.sch.id>
2016-12-25 11:25:27 +0700

    startActivity Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -1,13 +1,28 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    findViewById(R.id.buttonHitung)
+
+    setOnClickListener(new View.OnClickListener() {
+        @Override
+        publicvoid onClick (View v)
+        {
+            startActivity(new Intent(MainActivity.this, HasilActivity.class));
+        }
+    }
+
+    .
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    )
 }
